7fe7e2088a7b42d87ae75b1017c7d20e8f4e5202
asus <s.pashkovskiy08@gmail.com>
2019-09-01 16:21:16 +0300

    Question_6

---
+++ src/Exercise1/Question_6.java
@@ -0,0 +1,8 @@
+package Exercise1;
+
+public class Question_6 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 255; i++)
+        System.out.println(i + " - " + (char)i);
+    }
+}
